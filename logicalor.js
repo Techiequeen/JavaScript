3f40bdd55c43cfc1984a3abc86b6c5c33557cc09
Techiequeen <peacembaneme3@gmail.com>
2022-10-31 03:45:25 +0100

    practice

---
+++ logicalor.js
@@ -0,0 +1,6 @@
+// You can think of the ! operator as a switch, which flips the evaluated boolean value from true to false and from false to true.
+
+var petHungry = true;
+console.log('Feeding the pet');
+console.log("pet is hungry: ", !petHungry);
+console.log(petHungry);
