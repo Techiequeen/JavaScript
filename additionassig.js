f0484691b76924c6cb532a7a3c6ac60a8bd6e71c
Techiequeen <peacembaneme3@gmail.com>
2022-10-31 04:22:57 +0100

    practice

---
+++ additionassig.js
@@ -0,0 +1,17 @@
+// count the number of overtime hours worked in a week.
+
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri); // 9
+
+// you can simplify the above code using the addition assignment operator.
+
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime += 2;
+overtime += 3;
+console.log(overtime); // 9
