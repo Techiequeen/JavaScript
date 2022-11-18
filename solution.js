3868858e43d5d1a7b3f55b7d94ada3f4fe11893e
Techiequeen <peacembaneme3@gmail.com>
2022-11-18 21:11:14 +0100

    practice

---
+++ solution.js
@@ -0,0 +1,39 @@
+// Task 1
+
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy() { for (peer of dairy) { console.log(peer);}}
+
+logDairy();
+
+// Task 2
+
+const animal = { canJump: true };
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {    for (prop of Object.keys(bird)) {      
+
+  console.log(`${prop}: ${bird[prop]}`);    
+
+}
+
+}birdCan();
+
+// Task 3
+
+function animalCan() {    
+
+for (prop in bird) {        
+
+console.log(`${prop}: ${bird[prop]}`);   
+
+ }
+
+}
+
+animalCan();
